395b49270ada434892c0fe36e14f7c95ca41df6e
Joseph Irwin <joseph.irwin.gt@gmail.com>
2014-03-13 06:00:20 +0900

    Add to emacs config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,9 +1,25 @@
-;;; this loads the package manager
+(require 'cl)
 (require 'package)
 
-;;; here there's a variable named package-archives, and we are adding the MELPA repository to it
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-;;; loads packages and activates them
 (package-initialize)
+
+(setq packages '(evil markdown-mode monokai-theme))
+
+(defun ensure-package (package)
+    (unless (package-installed-p package)
+      (package-refresh-contents)
+      (package-install package)))
+
+(mapcar 'ensure-package packages)
+
+
+(require 'evil)
+(evil-mode t)
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+(load-theme 'monokai t)

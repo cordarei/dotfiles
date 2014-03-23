d396a3d3bce4bd2651f6ad6f657ebf9d4764af51
Joseph Irwin <joseph.irwin.gt@gmail.com>
2014-03-23 16:04:37 +0900

    Refactor init.el using use-package

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,35 +1,63 @@
+;; setup for requiring and installing packages
+
 (require 'cl)
 (require 'package)
 
+;; add extra ELPA archives
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
+;; initialize package system
 (package-initialize)
 
-(setq packages '(evil markdown-mode monokai-theme))
+;; get list of packages if not present
+(unless package-archive-contents
+  (package-refresh-contents))
 
-(defun ensure-package (package)
-    (unless (package-installed-p package)
-      (package-refresh-contents)
-      (package-install package)))
+;; make sure use-package is installed (we'll use it to install other packages)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-(mapcar 'ensure-package packages)
-
-
-(require 'evil)
-(evil-mode t)
+;; make use-package available
+(require 'use-package)
 
 
+;; set up individual packages
+
+;; evil-mode
+(use-package evil
+  :ensure evil
+  :config
+  (progn (evil-mode 1)))
+
+;; markdown-mode
+(use-package markdown-mode
+  :ensure markdown-mode
+  :commands markdown-mode)
+
+;; monokai theme
+(use-package monokai-theme
+  :ensure monokai-theme
+  :config
+  (progn (load-theme 'monokai t)))
+
+
+;; set up interface customizations
+
+;; behavior:
+
+(setq make-backup-files nil)
+
+;; keybindings:
+
+;; for Windows
 (global-set-key [M-kanji] 'ignore)
 (global-set-key [kanji] 'ignore)
 
 (define-key key-translation-map [?\C-h] [?\C-?])
 (global-set-key (kbd "M-h") 'help-command)
 
-(setq make-backup-files nil)
+;; GUI settings:
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-
-
-(load-theme 'monokai t)
